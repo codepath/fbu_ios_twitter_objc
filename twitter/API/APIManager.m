f43b5d80a3da34ede06703e9b889b5c6a5f25fc4
Timothy Lee <tim@codepath.com>
2018-07-01 15:43:09 -0700

    Inject compile error to make it more obvious to replace the api key/secret

--- twitter/API/APIManager.m
+++ twitter/API/APIManager.m
@@ -9,8 +9,8 @@
 #import "APIManager.h"
 
 static NSString * const baseURLString = @"https://api.twitter.com";
-static NSString * const consumerKey = @"YOUR_CONSUMER_KEY";
-static NSString * const consumerSecret = @"YOUR_CONSUMER_SECRET";
+static NSString * const consumerKey = // Enter your consumer key here
+static NSString * const consumerSecret = // Enter your consumer secret here
 
 @interface APIManager()
 
